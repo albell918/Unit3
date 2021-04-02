4fec863d65c311150e29e7b4d0cb98607245ba68
Alex <albell.916@gmail.com>
2021-04-02 23:02:44 +0300

    t3

--- src/MyTaxi/View.java
+++ src/MyTaxi/View.java
@@ -16,76 +16,52 @@ import java.util.stream.Stream;
 public class View {
     public static void main(String[] args) {
 
-//        TaxiPerKm t1r = new TaxiPerKm(1,"AZ-116","Volga-111",12.0,25.0);
-//        TaxiPerKm t2r = new TaxiPerKm(2,"Es-226-AN","Renault 11",9.0,25.0);
-//        TaxiPerTime t1t = new TaxiPerTime(3,"AA-222","ZAZ-101",10,20.0);
-
-        TaxiPerKm tpkm1 = new TaxiPerKm(10,"Az-111","ZAZ-1103",25,7,2.5,4);
+        TaxiPerKm tpkm1 = new TaxiPerKm(10, "Az-111", "ZAZ-1103", 25, 7, 2.5, 4);
 
         List<ICalculate> list = new ArrayList<>(
                 Arrays.asList(
 
-                    new TaxiPerKm(1,"Az-111","ZAZ-1103",25,7,2.5,4),
-                    new TaxiPerKm(2,"Az-111","ZAZ-1103",25,7,2.5,3),
-                    new TaxiPerKm(3,"XA-622-CC","Daewoo Lanos",30,7,2.5,15),
-                    new TaxiPerKm(4,"XA-622-CC","Daewoo Lanos",30,7,2.5,8.5),
-                    new TaxiPerKm(5,"AX79-91","Renault 11",20,6,2,5.5),
-                    new TaxiPerKm(6,"AX79-91","Renault 11",20,6,2,10.25),
-                    new TaxiPerTime(7,"AX82-17-NM","Renault Logan",84,125,4.0),
-                    new TaxiPerTime(8,"AX19-17-KI","VAZ-09",80,115,4.0),
-                    new TaxiPerKm(9,"XA-622-CC","Daewoo Lanos",30,7,2.5,8.0)
+                        new TaxiPerKm(1, "Az-111", "ZAZ-1103", 25, 7, 2.5, 4),
+                        new TaxiPerKm(2, "Az-111", "ZAZ-1103", 25, 7, 2.5, 3),
+                        new TaxiPerKm(3, "XA-622-CC", "Daewoo Lanos", 30, 7, 2.5, 15),
+                        new TaxiPerKm(4, "XA-622-CC", "Daewoo Lanos", 30, 7, 2.5, 8.5),
+                        new TaxiPerKm(5, "AX79-91", "Renault 11", 20, 6, 2, 5.5),
+                        new TaxiPerKm(6, "AX79-91", "Renault 11", 20, 6, 2, 10.25),
+                        new TaxiPerTime(7, "AX82-17-NM", "Renault Logan", 84, 125, 4.0),
+                        new TaxiPerTime(8, "AX19-17-KI", "VAZ-09", 80, 115, 4.0),
+                        new TaxiPerKm(9, "XA-622-CC", "Daewoo Lanos", 30, 7, 2.5, 8.0)
 
 
                 )
         );
 
-
-
         double totalRevenue = list.stream().mapToDouble(ICalculate::calcRevenue).sum();
-//        double minRevenuePerRide = list.stream().mapToDouble(ICalculate::calcRevenue).min();
-//        double maxRevenuePerRide = list.stream().mapToDouble(ICalculate::calcRevenue).max();
+        double minRevenuePerRide = list.stream().mapToDouble(ICalculate::calcRevenue).min().getAsDouble();
+        double maxRevenuePerRide = list.stream().mapToDouble(ICalculate::calcRevenue).max().getAsDouble();
+        list.stream().filter(ride -> ride.calcRevenue() > 50).forEach(car -> System.out.println("Rides with 50+ revenue :" + car + "Revenue :" + car.calcRevenue()));
+        System.out.println("---------------------------------------------------------------------------------------------------------------------");
+        list.stream().filter(TaxiPerTime.class::isInstance).forEach(car -> System.out.println("Time Fee: " + car));
+        System.out.println("---------------------------------------------------------------------------------------------------------------------");
+
+//        list.stream().filter(ride->ride.getClass().isInstance(new TaxiPerTime)).forEach();
 
 
+//     arrStream.filter(ride->ride.calcRevenue()>50).forEach(car-> System.out.println("Rides with 50+ revenue :"+car+"Revenue :"+car.calcRevenue()));
 
-        Stream<ICalculate> arrStream = Stream.of(new TaxiPerKm(1,"Az-111","ZAZ-1103",25,7,2.5,4),
-                new TaxiPerKm(2,"Az-111","ZAZ-1103",25,7,2.5,3),
-                new TaxiPerKm(3,"XA-622-CC","Daewoo Lanos",30,7,2.5,15),
-                new TaxiPerKm(4,"XA-622-CC","Daewoo Lanos",30,7,2.5,8.5),
-                new TaxiPerKm(5,"AX79-91","Renault 11",20,6,2,5.5),
-                new TaxiPerKm(6,"AX79-91","Renault 11",20,6,2,10.25),
-                new TaxiPerTime(7,"AX82-17-NM","Renault Logan",84,125,4.0),
-                new TaxiPerTime(8,"AX19-17-KI","VAZ-09",80,115,4.0),
-                new TaxiPerKm(9,"XA-622-CC","Daewoo Lanos",30,7,2.5,8.0));
-
-
-
-
-        arrStream.filter(ride->ride.calcRevenue()>50).forEach(car-> System.out.println("Rides with 50+ revenue :"+car+"Revenue :"+car.calcRevenue()));
-
-
-        System.out.println(" Total revenue is :"+totalRevenue);
-
-
-
-//        list.stream().filter(car->car.getClass()==TaxiPerTime);
-
-
-
-
-//        System.out.println(" Total revenue is :"+totalRevenue+ " Max per ride :"+maxRevenuePerRide+" Min per ride :"+minRevenuePerRide);
-
-
-// R logan 12/100
-//        private double fuelPerHour;
-//        private double feePerHour;
-//        private double hours;
-
-//        super(raceId, craNumber, carModel);
-//        this.basicFee = basicFee;
-//        this.feePerKilometer = feePerKilometer;
-//        this.fuelPerKM = fuelPerKM;
-//        this.kilometres = kilometres;
+        System.out.println(" Total revenue is :" + totalRevenue + " Max per ride :" + maxRevenuePerRide + " Min per ride :" + minRevenuePerRide);
 
     }
 
 }
+
+//        Rides with 50+ revenue :TaxiPerKm{Taxi{rideId=3, craNumber='XA-622-CC', carModel='Daewoo Lanos'}basicFee=30.0, feePerKilometer=7.0, fuelPerKM=2.5, kilometres=15.0}Revenue :97.5
+//        Rides with 50+ revenue :TaxiPerKm{Taxi{rideId=4, craNumber='XA-622-CC', carModel='Daewoo Lanos'}basicFee=30.0, feePerKilometer=7.0, fuelPerKM=2.5, kilometres=8.5}Revenue :68.25
+//        Rides with 50+ revenue :TaxiPerKm{Taxi{rideId=6, craNumber='AX79-91', carModel='Renault 11'}basicFee=20.0, feePerKilometer=6.0, fuelPerKM=2.0, kilometres=10.25}Revenue :61.0
+//        Rides with 50+ revenue :TaxiPerTime{Taxi{rideId=7, craNumber='AX82-17-NM', carModel='Renault Logan'}fuelPerHour=84.0, feePerHour=125.0, hours=4.0}Revenue :164.0
+//        Rides with 50+ revenue :TaxiPerTime{Taxi{rideId=8, craNumber='AX19-17-KI', carModel='VAZ-09'}fuelPerHour=80.0, feePerHour=115.0, hours=4.0}Revenue :140.0
+//        Rides with 50+ revenue :TaxiPerKm{Taxi{rideId=9, craNumber='XA-622-CC', carModel='Daewoo Lanos'}basicFee=30.0, feePerKilometer=7.0, fuelPerKM=2.5, kilometres=8.0}Revenue :66.0
+//        ---------------------------------------------------------------------------------------------------------------------
+//        Time Fee: TaxiPerTime{Taxi{rideId=7, craNumber='AX82-17-NM', carModel='Renault Logan'}fuelPerHour=84.0, feePerHour=125.0, hours=4.0}
+//        Time Fee: TaxiPerTime{Taxi{rideId=8, craNumber='AX19-17-KI', carModel='VAZ-09'}fuelPerHour=80.0, feePerHour=115.0, hours=4.0}
+//        ---------------------------------------------------------------------------------------------------------------------
+//        Total revenue is :720.25 Max per ride :164.0 Min per ride :38.5
